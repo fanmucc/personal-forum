1f09761d82afc52462399850d5dd950be4b04441
fanmu <fanmucc@yeah.net>
2020-06-22 15:34:36 +0800

    双仓库测试

--- src/router/index.js
+++ src/router/index.js
@@ -29,7 +29,7 @@ router.beforeEach((to, form, next) => {
   // 获取token 如果没有则跳转出登录header
   let token = window.localStorage.token
   if (!token) {
-    console.log('1234')
+    console.log('12345')
     next()
   } else {
     next()
